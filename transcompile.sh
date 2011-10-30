583f785d79c715f6e9e520be33faacdabf3d4673
Christoph Lipautz <christoph@lipautz.org>
2011-10-30 13:38:40 +0100

    adding minified

--- transcompile.sh
+++ transcompile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-hash coffee 2>&- || { echo >&2 "coffee-script is needed for transcompilation see http://jashkenas.github.com/coffee-script/"; exit 1; }
 coffee -o lib/ -c src/
+uglify lib/jquery.behaviour.js > lib/jquery.behaviour.min.js
